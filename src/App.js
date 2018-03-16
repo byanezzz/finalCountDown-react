af3ab6add84dfe49bed4f4f83b4a1d325a23916b
carrz <carla.zumaran@gmail.com>
2018-03-15 22:20:17 -0300

    Estructura de container y app lista, arreglo de estilos y anexo de flexbox y material-ui

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import AppBar from 'material-ui/AppBar';
 import './App.css';
 import Header from './components/Header/Header';
 import SearchForm from './components/SearchForm/SearchForm';
+/* import SearchContainer from './containers/SearchContainer/SearchContainer'; */
+
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header/>
-        <SearchForm/>
-      </div>
-    );
-  }
-}
+      <MuiThemeProvider>
+        <Grid>
+          <Row>
+            <Col xs={12}>
+              <AppBar title='WikiSearch'/>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} md={6}>
+              <SearchForm></SearchForm>
+              {/* <SearchContainer></SearchContainer> */}
+            </Col>
+            <Col xs={12} md={6}>
+              {/* <ContentContainer></ContentContainer> */}
+            </Col>
+          </Row>
+        </Grid>
+      </MuiThemeProvider>
+      );
+  };
+};
 
 export default App;
